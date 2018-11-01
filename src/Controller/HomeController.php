c2a41544777c7719522146319bca324e99b3f21b
Svetlana Muraveckaja-Odincova <svetlana.mur.85@gmail.com>
2018-11-01 15:50:38 +0200

    reset

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,47 +2,25 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller
 {
+
+
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
-
-        return $this->render('home/index.html.twig', [
-             'projects' => [
-             'Po pamokų'=>[
-                  "mentor" => "Tomas",
-                  "members" => ["Elena","Justė","Deimantas"]
-            ],
-             'Tech Guide'=>[
-                "mentor" => "Sergej",
-                "members" => ["Matas","Martynas"]
-            ],
-            'Kelionės draugas'=>[
-                     "mentor" => "Rokas",
-                     "members" => ["Zbignev","Aistė"]
-                 ],
-            'Wish A Gift'=>[
-                     "mentor" => "Aistis",
-                     "members" => ["Nerijus","Olga"]
-                 ],
-            'Mums pakeliui'=>[
-                     "mentor" => "Paulius",
-                     "members" => ["Eglė","Svetlana"]
-                 ],
-            'Motyvacinė platforma'=>[
-                     "mentor" => "Audrius",
-                     "members" => ["Viktoras","Airidas"]
-                 ]
-
-          ]
-        ]);
-
-
+       return $this->render(
+           'home/index.html.twig'
+       );
     }
+
+
 }
